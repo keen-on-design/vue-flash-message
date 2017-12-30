b7c8532f1b9bdb553d9306b6adcdc1ce9662d5c7
Nikita <maccuro.media@gmail.com>
2017-12-30 19:13:01 +0300

    add extract text plugin

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -1,13 +1,14 @@
-'use strict'
-const path = require('path')
-const utils = require('./utils')
-const webpack = require('webpack')
-const config = require('../config')
-const merge = require('webpack-merge')
-const baseWebpackConfig = require('./webpack.base.conf')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
-const portfinder = require('portfinder')
+'use strict';
+const path = require('path');
+const utils = require('./utils');
+const webpack = require('webpack');
+const config = require('../config');
+const merge = require('webpack-merge');
+const baseWebpackConfig = require('./webpack.base.conf');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin');
+const portfinder = require('portfinder');
 
 const HOST = process.env.HOST
 const PORT = process.env.PORT && Number(process.env.PORT)
@@ -33,6 +34,9 @@ module.exports = merge(baseWebpackConfig, {
       'process.env': {
         NODE_ENV: JSON.stringify('development')
       }
+    }),
+    new ExtractTextPlugin({
+      filename: 'vue-flash-message.min.css',
     }),
     new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
