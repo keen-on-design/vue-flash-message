e520a091a1c3269e91991bbda24576fbd9642053
Nikita <maccuro.media@gmail.com>
2017-12-30 19:13:11 +0300

    add button type select

--- dev/components/SpawnButton.vue
+++ dev/components/SpawnButton.vue
@@ -1,13 +1,29 @@
 <template>
-  <button v-on:click="clickButton">flash it babe</button>
+  <section>
+    <select v-model="messageType">
+      <option selected>success</option>
+      <option>error</option>
+      <option>warning</option>
+      <option>info</option>
+    </select>
+    <span>Selected: {{ messageType }}</span>
+    <hr/>
+    <button v-on:click="clickButton">flash it babe</button>
+  </section>
 </template>
 
 <script>
 export default {
   name: 'spawn-button',
+  data() {
+    return {
+      messageType: 'error',
+    };
+  },
+
   methods: {
     clickButton() {
-      this.flash('random modal', 'error', {
+      this.flash('random modal', this.messageType, {
         timeout: 3000,
       });
     }
