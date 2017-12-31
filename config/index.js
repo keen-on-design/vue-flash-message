16429a77445ed02c07f8190e54f57dc4df0cedce
Nikita <maccuro.media@gmail.com>
2017-12-31 08:08:26 +0300

    .

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, '../dev/index.html'),
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
